a77d0949185715333ffa1442b3864fb7738729eb
chetan <chetanboraselit@gmail.com>
2023-01-01 15:48:26 +0530

    lombok removed

--- src/main/java/com/cognizant/chatbot/dto/Message.java
+++ src/main/java/com/cognizant/chatbot/dto/Message.java
@@ -1,15 +1,36 @@
 package com.cognizant.chatbot.dto;
 
 import com.cognizant.chatbot.entity.FAQ;
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
 public class Message {
     private Long id;
     private String content;
     private Date timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public static Message mapFromFAQ(FAQ q){
         Message message = new Message();
--- src/main/java/com/cognizant/chatbot/entity/FAQ.java
+++ src/main/java/com/cognizant/chatbot/entity/FAQ.java
@@ -1,16 +1,10 @@
 package com.cognizant.chatbot.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.Nationalized;
-
-import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "TblFAQ")
-@Data
 public class FAQ {
-
     @Id
     private Long id;
 
@@ -20,4 +14,28 @@ public class FAQ {
     @Lob
     @Column(length = Integer.MAX_VALUE)
     private String answer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
